52a7c7c05496bcfcf2726e9c48383a47a67bfd64
FrancoDalla <francodalladg@outlook.com>
2026-06-29 16:09:34 -0300

    avances

--- codigo-esp32/sketch_jun23a/config.h
+++ codigo-esp32/sketch_jun23a/config.h
@@ -5,12 +5,12 @@
 #define PIN_DHT 4
 
 /*Sección de configuraciones por defecto de espera*/
-#define DEFAULT_SLEEP_TIME 3 //horas
+#define DEFAULT_SLEEP_TIME 6
 
 #define SSID_WIFI "Cj"
 #define PASS_WIFI "poroto22"
 
-#define MQTT_IP_DEFAULT  "10.34.86.217"
+#define MQTT_IP_DEFAULT  "10.177.251.238"
 #define MQTT_PORT_DEFAULT  1883
 
 #define MQTT_CLIENT_ID  "Carlitos"
